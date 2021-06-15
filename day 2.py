b6adf36f26a19875412fd99b175404e55cd3fa85
Rashid3601 <rashidsadi55@gmail.com>
2021-06-15 17:06:23 +0100

    Add files via upload

---
+++ day 2.py
@@ -0,0 +1,76 @@
+#learning about variables and making them
+my_name = "Rashid"
+my_age = 26
+Learner = True
+print(my_name) 
+fav_drink = "hot chocolate"
+print(fav_drink)
+print("my faviorate drink is", fav_drink)
+name = "Rashid"
+fav_drink ="hot chocolate"
+print("{} faviorate drink is {}".format(name,fav_drink))
+print(f"{name} faviorate drink is {fav_drink}")
+#addition and maths time now
+#Then add 2 to the value of i
+i = 10
+i += 2
+# i = 12
+num = 0
+# num = num + 100
+num += 100  # these are the same but one is shorter
+num /= 20  # these are all shorter version of num = num plus or minus or divde etc
+num *= 10
+num -= 45
+print(num)
+his_name = "David"
+his_age = "28"
+Fav_Colour = "Blue"
+print(f"{his_name} Age is {his_age} and faviorate colour is {Fav_Colour}")
+Today_breakfast= "Cereal and milk"
+Today_lunch="Steak and Chips"
+Today_dinner="pizza"
+print(f"{my_name} breakfast today is {Today_breakfast} and lunch today is {Today_lunch} and Today dinner is {Today_dinner} ")
+Today_breakfast="egg and mushrooms"
+Today_lunch="pasta and beef"
+Today_dinner="noodles" 
+print(f"{my_name} breakfast today is {Today_breakfast} and lunch today is {Today_lunch} and Today dinner is {Today_dinner} ")
+print
+A_b = "x"
+b_a = "o"
+print ("---------------------------------")
+print (" |        |         |           |")
+print (f" |    {A_b}   |   {b_a}     |           |")
+print (" |        |         |           |")
+print ("---------------------------------")
+print (" |        |         |           |")
+print (f" |    {A_b}   |   {b_a}     |           |")
+print (" |        |         |           |")
+print ("---------------------------------")
+print (" |        |         |           |")
+print (f" |    {b_a}   |         |           |")
+print (" |        |         |           |")
+print ("---------------------------------")
+music = "Rock" #THIS IS THE VARIABLE
+# == != COMPARISSON OPERATORS
+#if music == "Afrobeats": #THE == MEANS IS IT
+    #print("Yes I!!, Afrobeats are BANGING")
+#elif music == "Mozart": #THIS IS THE NEXT CONDITION IF 1ST NOT TRUE
+    #print("Yes, I am in balance") #PRINTED 2ND RESPONSE
+#else:
+   #print("Disasterous Sounds, SKIP IT") #IF NONE OF OTHER CONDITIONS ARE TRUE
+age = 26
+country = "UK" #note:make a string with speechmarks
+if age > 17 and country=="UK":
+    print("I can serve you")
+elif age < 17:
+    print("You must leave")
+else:
+    print("leave right now")
+place = "MCR" 
+weather = "Cloudy"
+day = "Saturday"
+if day =="saturday" or day == "Sunday":
+    print("It's weekend!")
+else:
+    print("When's weekend?")
+
